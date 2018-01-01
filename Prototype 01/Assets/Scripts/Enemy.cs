99bcca42c3c1626c225a53b54ab3c3d6f9660ce7
yuvalkarif3 <34835592+yuvalkarif3@users.noreply.github.com>
2018-01-01 15:49:53 +0200

    Swap Weapon

--- Prototype 01/Assets/Scripts/Enemy.cs
+++ Prototype 01/Assets/Scripts/Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
-
+    [System.Serializable]
     public class EnemyStats
     {
         public int health = 100;
--- Prototype 01/Assets/Scripts/Player.cs
+++ Prototype 01/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    [System.Serializable]
     public class PlayerStats
     {
         public int health = 100;
