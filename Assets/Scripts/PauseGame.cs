2c352d91b4095bb2938413f6dd5f38d7f7393310
TommerFed <34835609+TommerFed@users.noreply.github.com>
2018-01-29 08:42:23 +0200

    my part with pause menu

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour {
+
+	public Transform canvas;
+
+	// Update is called once per frame
+	void Update () {
+		pause();
+	}
+	public void pause()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (canvas.gameObject.activeInHierarchy == false) {
+				canvas.gameObject.SetActive(true);
+				Time.timeScale = 0;
+
+			} 
+			else {
+				canvas.gameObject.SetActive (false);
+				Time.timeScale = 1;
+			}
+		}
+	}
+}
