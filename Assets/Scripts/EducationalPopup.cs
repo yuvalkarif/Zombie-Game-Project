3546c7254f28dab939ffd5e95163dfac300fe029
TommerFed <34835609+TommerFed@users.noreply.github.com>
2018-04-11 15:37:15 +0300

    Educational popup needs fix

---
+++ Assets/Scripts/EducationalPopup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EducationalPopup : MonoBehaviour {
+
+	public Transform EducationalCanvas;
+
+	//TODO : add a list of facts / questions 
+	public int waveNum = 0;
+	WaveSpawner ws;
+
+	void Start()
+	{
+		ws =  GameObject.FindGameObjectWithTag("GameMaster").GetComponent<WaveSpawner>();
+		waveNum = ws.WaveNumber;
+	}
+	public void Popup()
+	{
+		if (EducationalCanvas.gameObject.activeInHierarchy == false) {
+			EducationalCanvas.gameObject.SetActive (true);
+			Time.timeScale = 0;
+			
+		} 
+		else {
+			EducationalCanvas.gameObject.SetActive (false);
+			Time.timeScale = 1;
+			
+
+
+		}
+	}
+	void Update()
+	{
+		
+	}
+}
